68f020ec6ae3b374e759782234fe50ec5a216c28
Yinan Li <gabriel.li5566@gmail.com>
2022-08-11 12:23:17 +0300

    update style sheet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'antd/dist/antd.min.css'
-import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
+import App from './App'
+
+import 'antd/dist/antd.min.css'
+import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
