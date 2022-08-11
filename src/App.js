85561d508fd0c8679168594cf571dfbcdd70a59f
Yinan Li <gabriel.li5566@gmail.com>
2022-08-11 11:53:20 +0300

    configure basic routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+import Login from 'pages/Login'
+import NotFound from 'pages/NotFound'
+
 function App() {
-  return <div>APP</div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes: */}
+          <Route path="/" element={<Navigate to="/login" replace />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
