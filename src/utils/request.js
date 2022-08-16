0648043e0649463c60c00fd9eb678fbdc9daddb1
Yinan Li <gabriel.li5566@gmail.com>
2022-08-16 13:49:31 +0300

    config interceptor

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,4 +7,33 @@ const request = axios.create({
   timeout: 5000,
 })
 
+// Add a request interceptor
+request.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// Add a response interceptor
+request.interceptors.response.use(
+  function (response) {
+    // Do something with response data
+    return response.data
+  },
+  function (error) {
+    // Do something with response error
+    if (!error.response) {
+      return Promise.reject(error)
+    }
+    if (error.response.status === 401) {
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default request
