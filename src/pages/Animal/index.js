7f215ba27121fe3c842abcb86dc1b8f5eb3ebd00
sami-alt <goodfellatoo@gmail.com>
2022-08-16 15:23:35 +0300

    Animal feature working with json server

--- src/pages/Animal/index.js
+++ src/pages/Animal/index.js
@@ -1,5 +1,6 @@
 import { getAnimal } from "api/animal"
 import React, { useEffect, useState } from "react"
+import {  Descriptions } from "antd"
 
 const Animal = (props) => {
     const [animal, setAnimal] = useState(null)
@@ -15,7 +16,33 @@ const Animal = (props) => {
 
     return (
         <div>
-            <p>Tesy tunnus: {animal.tesyID}</p>
+            <Descriptions
+                title="Eläimen tiedot"
+                bordered
+                column={{
+                    xxl:1
+                }}>
+                    <Descriptions.Item label='Tesy tunnus '> {animal.tesyID}</Descriptions.Item>
+                    <Descriptions.Item label="Laji">{animal.species}</Descriptions.Item>
+                    <Descriptions.Item label='Napattu'> {animal.inDate}</Descriptions.Item>
+                    <Descriptions.Item label="Saapunut Tesylle">{animal.inTesyDate}</Descriptions.Item>
+                    <Descriptions.Item label="Poistunut Tesyltä">{animal.outOfTesyDate}</Descriptions.Item>
+                    <Descriptions.Item label="Mistä">{animal.fromWhere}</Descriptions.Item>
+                    <Descriptions.Item label="populaatio">{animal.fromWhere}</Descriptions.Item>
+                    <Descriptions.Item label="syy">{animal.reason}</Descriptions.Item>
+                    <Descriptions.Item label="Nimi">{animal.callingNameOfTheAnimal}</Descriptions.Item>
+                    <Descriptions.Item label="Mikrosiru numero">{animal.microchipNumber}</Descriptions.Item>
+                    <Descriptions.Item label="Ikä ryhmä">{animal.animalAge}</Descriptions.Item>
+                    <Descriptions.Item label="Rotu">{animal.breedAnimal}</Descriptions.Item>
+                    <Descriptions.Item label="Väri">{animal.animalColor}</Descriptions.Item>
+                    <Descriptions.Item label="Syy eutanasialle">{animal.breedAnimal}</Descriptions.Item>
+                    <Descriptions.Item label="Sey tilasto">{animal.seyStatistics}</Descriptions.Item>
+                    <Descriptions.Item label="Muuta huomautettavaa">{animal.extraNotes}</Descriptions.Item>
+                    
+                    <Descriptions.Item label="Annetut hoidot">{animal.treatmentList}</Descriptions.Item>
+                   
+
+            </Descriptions>
         </div>
     )
 }
