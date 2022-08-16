4de791144b992e1ecde0cd29c847d59496afae31
sami-alt <goodfellatoo@gmail.com>
2022-08-16 14:27:49 +0300

    fetch one animal

--- src/api/animal.js
+++ src/api/animal.js
@@ -1,11 +1,7 @@
 import request from 'utils/request'
 
-/**
- * request for login
- * @param {string} email
- * @param {string} password
- * @returns
- */
+
+
 export const getAnimal = (id) => {
   return request({
     method: 'GET',
--- src/pages/Animal/index.js
+++ src/pages/Animal/index.js
@@ -1,5 +1,23 @@
-const Animal = () => {
-  return <div>Animal Page</div>
+import { getAnimal } from "api/animal"
+import React, { useEffect, useState } from "react"
+
+const Animal = (props) => {
+    const [animal, setAnimal] = useState(null)
+    useEffect(() => {
+        getAnimal(1).then(_ => setAnimal(_.data))
+    }, [])
+
+    console.log(animal)
+
+    if (!animal) {
+        return 'Ladataan...'
+    }
+
+    return (
+        <div>
+            <p>Tesy tunnus: {animal.tesyID}</p>
+        </div>
+    )
 }
 
 export default Animal
