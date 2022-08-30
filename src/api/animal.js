7568525e764e5a74f45d6a76c9993dd7063f23ed
Sami Räihä <sami.k.raiha@utu.fi>
2022-08-30 19:43:53 +0300

    Fix so api path is in base URL

--- src/api/animal.js
+++ src/api/animal.js
@@ -3,13 +3,13 @@ import request from 'utils/request'
 export const getAnimal = (id) => {
   return request({
     method: 'GET',
-    url: '/animals/' + id,
+    url: '/animal/' + id,
   })
 }
 
 export const getAnimals = () => {
   return request({
     method: 'GET',
-    url: '/animals/',
+    url: '/animal',
   })
 }
