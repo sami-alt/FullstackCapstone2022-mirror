e94a763c93030736beced24aae457f2c25dfafe2
Sami Räihä <sami.k.raiha@utu.fi>
2022-09-06 02:15:48 +0300

    Configure moment to use Finnish locales

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
+import 'moment/locale/fi.js'
+import moment from 'moment'
 
 import 'antd/dist/antd.min.css'
 import './index.scss'
 
 import { persistor } from 'store/index'
 import { PersistGate } from 'redux-persist/lib/integration/react'
+
+moment.locale('fi')
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
