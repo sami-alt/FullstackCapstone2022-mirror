6a6e129785e2eec82c170b157ca3f4b0ad582b76
Sami Räihä <sami.k.raiha@utu.fi>
2022-09-06 01:18:23 +0300

    Fix date handling in update form

--- src/pages/Animal/SingleAnimalDetailView.js
+++ src/pages/Animal/SingleAnimalDetailView.js
@@ -1,23 +1,50 @@
-import { Descriptions, Modal } from 'antd';
+import { Descriptions, Modal, DatePicker } from 'antd';
 import React, {useState} from 'react';
 import { editAnimal } from 'api/animal';
+import moment from 'moment'
 import 'pages/Animal/animal.scss'
+
+const parseValue = (rawValue, isDateField) => {
+    if (!rawValue) {
+        return ''
+    }
+    if (isDateField) {
+        const momentValue =  moment(rawValue)
+        if (momentValue.isValid()) {
+            return momentValue
+        }
+    }
+    return rawValue
+}
 
 const LabelOrInput = (props) => {
     const {field, commonProps: {animal, setPatch}} = props
 
-    const value = animal[field]
+    const rawValue = animal[field]
+    const isDateField = field.endsWith('Date')
+    const value = parseValue(rawValue, isDateField)
 
-    const onChange = (event) => {
-        const newValue = event.target.value
-
+    const onChangeValue = (newValue) => {
         setPatch(oldAnimalPatch => ({
             ...oldAnimalPatch,
             [field]: newValue,
         }))
     }
 
-    return <input className="viewable" type="text" value={value || ''} onChange={onChange} />
+    if (isDateField) {
+        const onChange = (momentValue) => {
+            const asText = momentValue?.format('YYYY-MM-DD') || null
+            onChangeValue(asText)
+        }
+
+        return <DatePicker value={value} onChange={onChange} />
+    } else {
+        const onChange = (event) => {
+            onChangeValue(event.target.value)
+        }
+
+        return <input className="viewable" type="text" value={value || ''} onChange={onChange} />
+    }
 }
 
 
@@ -66,7 +93,7 @@ const SingleAnimalDetailView = ({ animal: savedAnimal, visible, setVisible, onAn
                     <Descriptions.Item label="Laji"><span>{animal.species?.name}</span></Descriptions.Item>
                     <Descriptions.Item label='Napattu'><LabelOrInput commonProps={commonProps} field="inDate"/></Descriptions.Item>
                     <Descriptions.Item label="Saapunut Tesylle"><LabelOrInput commonProps={commonProps} field="inTesyDate"/></Descriptions.Item>
-                    <Descriptions.Item label="Poistunut Tesyltä"><LabelOrInput commonProps={commonProps} field="outOfTesyDate"/></Descriptions.Item>
+                    <Descriptions.Item label="Poistunut Tesyltä"><LabelOrInput commonProps={commonProps} field="outTesyDate"/></Descriptions.Item>
                     <Descriptions.Item label="Mistä"><LabelOrInput commonProps={commonProps} field="fromWhere"/></Descriptions.Item>
                     <Descriptions.Item label="populaatio"><LabelOrInput commonProps={commonProps} field="population"/></Descriptions.Item>
                     <Descriptions.Item label="syy"><LabelOrInput commonProps={commonProps} field="reason"/></Descriptions.Item>
